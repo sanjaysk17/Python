c20dbf39e6139f3eb98f98f343a854f0da7d9df1
sanjaysk17 <sanjaykumar6369k@gmail.com>
2026-03-09 16:15:13 +0530

    List Done

---
+++ list.py
@@ -0,0 +1,14 @@
+food=["Apple","Strawberry","Pizza","Hamburger","Icecream"]
+print(food)
+food.append("Coffee")##To append the item in last
+print(food)
+food.pop()##To remove the last element
+print(food)
+food.insert(2,"Tea")##to insert at Desired Index
+print(food)
+food.remove("Apple")##To remove Specific item
+print(food)
+food.sort()##To sort
+print(food)
+food.clear()##To clear all the list
+print(food)
