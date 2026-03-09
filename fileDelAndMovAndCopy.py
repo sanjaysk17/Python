8e9d14d30e72082ab97914ce25769ed9726a1e78
sanjaysk17 <sanjaykumar6369k@gmail.com>
2026-03-09 15:56:16 +0530

    Moving,deleting and Copying was Done

---
+++ fileDelAndMovAndCopy.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+##Syntax
+#path="File_path"
+##os.remove(path)
+#os.rmdir(path)##Deleting a Folder
+#shutil.rmtree(path)
+####Remove all files and Folder in That Folder
+
+
+##src and destination are path or FIlename if exit in project folder
+
+"""
+shutil.copy(src,dest)  Copy content Only
+shutil.copyfile(src,des)##Copy a content.Permission as well as In destination Folder
+shutil.copy2(src,des) Copy content and Meta data
+"""
+
+##
+##os.replace(src,dir) Move a folder or file
